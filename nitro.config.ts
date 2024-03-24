aa3c28d6d7762c3005c773f351271568fdbdd444
ayodyln <78442693+ayodyln@users.noreply.github.com>
2024-03-24 17:49:49 -0600

    Add CORS headers to nitro.config.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -6,6 +6,11 @@ export default defineNitroConfig({
       cors: true,
       headers: {
         "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "multipart/form-data": "application/json",
       },
     },
   },
