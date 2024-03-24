504bbc13d8d812ee0520b9bffadd02e708ce7764
ayodyln <78442693+ayodyln@users.noreply.github.com>
2024-03-24 17:24:31 -0600

    Add CORS headers for Access-Control-Allow-Methods and Access-Control-Allow-Headers

--- nitro.config.ts
+++ nitro.config.ts
@@ -6,6 +6,8 @@ export default defineNitroConfig({
       cors: true,
       headers: {
         "Access-Control-Allow-Origin": process.env.CORS_ORIGIN,
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
       },
     },
   },
