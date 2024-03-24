4da2c9f2acacc2d7383ef455b0f7d85cf95faaae
ayodyln <78442693+ayodyln@users.noreply.github.com>
2024-03-24 17:15:32 -0600

    Refactor CORS handling in index.ts

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,11 +2,22 @@ import { corsHndlr } from "../../utils/cors";
 
 export default eventHandler({
   onRequest: (event) => {
-    const { respondWith } = event;
-    const cors = corsHndlr(getHeaders(event), [process.env.CORS_ORIGIN]);
-    if (!cors) respondWith(new Response("Origin Not Allowed", { status: 403 }));
+    const headers = getHeaders(event);
+
+    if (!headers["origin"]) {
+      event.respondWith(new Response("Missing Origin header", { status: 400 }));
+    }
+
+    const cors = corsHndlr(headers, [process.env.CORS_ORIGIN]);
+
+    if (!cors) {
+      event.respondWith(new Response("Not Allowed", { status: 403 }));
+    }
   },
-  onBeforeResponse: (event) => {},
+  onBeforeResponse: (event) => {
+    const headers = getHeaders(event);
+    // console.log("response", headers);
+  },
   handler: (event) => {
     const headers = getHeaders(event);
     // console.log(headers);
