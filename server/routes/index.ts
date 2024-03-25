e36dbc4956aa6a88569b232360bb59d5ae5d29ca
ayodyln <78442693+ayodyln@users.noreply.github.com>
2024-03-24 18:56:27 -0600

    Refactor request handling and add CORS support

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,4 +1,4 @@
-import { onRequest } from "../../utils/handlers";
+import { corsHndlr } from "../../utils/cors";
 
 export default eventHandler({
   onRequest,
@@ -13,3 +13,20 @@ export default eventHandler({
     return headers;
   },
 });
+
+function onRequest(event) {
+  const headers = getHeaders(event);
+  console.log("request", headers);
+  if (!headers["origin"]) {
+    event.respondWith(
+      new Response("Bad Request: Missing Origin Header", { status: 400 })
+    );
+  } else if (headers["origin"] !== "https://www.dylan-portfolio.dev") {
+    event.respondWith(new Response("Access Not Allowed", { status: 403 }));
+  }
+  const cors = corsHndlr(headers, ["https://www.dylan-portfolio.dev"]);
+  console.log(cors);
+  if (!cors) {
+    event.respondWith(new Response("Access Not Allowed", { status: 403 }));
+  }
+}
