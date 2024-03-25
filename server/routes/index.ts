ca8b60f689ad52958a50b9ebc34689de1147f6b9
ayodyln <78442693+ayodyln@users.noreply.github.com>
2024-03-24 18:55:51 -0600

    Refactor onRequest function in index.ts

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,22 +1,7 @@
-import { corsHndlr } from "../../utils/cors";
+import { onRequest } from "../../utils/handlers";
 
 export default eventHandler({
-  onRequest: (event) => {
-    const headers = getHeaders(event);
-    console.log("request", headers);
-    if (!headers["origin"]) {
-      event.respondWith(
-        new Response("Bad Request: Missing Origin Header", { status: 400 })
-      );
-    } else if (headers["origin"] !== "https://www.dylan-portfolio.dev") {
-      event.respondWith(new Response("Access Not Allowed", { status: 403 }));
-    }
-    const cors = corsHndlr(headers, ["https://www.dylan-portfolio.dev"]);
-    console.log(cors);
-    if (!cors) {
-      event.respondWith(new Response("Access Not Allowed", { status: 403 }));
-    }
-  },
+  onRequest,
   onBeforeResponse: (event) => {
     const headers = getHeaders(event);
     // console.log("response", headers);
