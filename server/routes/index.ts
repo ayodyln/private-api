1309464917daffc0216a9c71cf5d310a094028a9
ayodyln <78442693+ayodyln@users.noreply.github.com>
2024-03-24 19:24:31 -0600

    Remove unused code in index.ts

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,6 @@
 import { corsHndlr } from "../../utils/cors";
 
 export default eventHandler({
-  onRequest,
-  onBeforeResponse,
   handler: (event) => {
     const headers = getHeaders(event);
     // console.log(headers);
@@ -10,25 +8,3 @@ export default eventHandler({
     return headers;
   },
 });
-
-function onRequest(event) {
-  const headers = getHeaders(event);
-
-  if (!headers["origin"]) {
-    event.respondWith(
-      new Response("Bad Request: Missing Origin Header", { status: 400 })
-    );
-  } else if (headers["origin"] !== "https://www.dylan-portfolio.dev") {
-    event.respondWith(new Response("Access Not Allowed", { status: 403 }));
-  }
-
-  const cors = corsHndlr(headers, ["https://www.dylan-portfolio.dev"]);
-
-  if (!cors) {
-    event.respondWith(new Response("Access Not Allowed", { status: 403 }));
-  }
-}
-
-function onBeforeResponse(event) {
-  const headers = getHeaders(event);
-}
