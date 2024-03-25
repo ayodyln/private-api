bea8fb05a3ce192b5d4d6465fbc98f73e1a68091
ayodyln <78442693+ayodyln@users.noreply.github.com>
2024-03-24 19:28:34 -0600

    Update nitro.config.ts to allow POST requests

--- nitro.config.ts
+++ nitro.config.ts
@@ -6,7 +6,7 @@ export default defineNitroConfig({
       cors: true,
       headers: {
         "Access-Control-Allow-Origin": "https://www.dylan-portfolio.dev",
-        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Methods": "GET, POST",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
         "Content-Type": "application/json",
       },
