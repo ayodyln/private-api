6dde8573ce3796edb98005d34b6b4e9ea3c20b00
ayodyln <78442693+ayodyln@users.noreply.github.com>
2024-03-24 18:59:01 -0600

    Remove unnecessary console.log statements

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -13,7 +13,7 @@ export default eventHandler({
 
 function onRequest(event) {
   const headers = getHeaders(event);
-  console.log("request", headers);
+
   if (!headers["origin"]) {
     event.respondWith(
       new Response("Bad Request: Missing Origin Header", { status: 400 })
@@ -21,8 +21,9 @@ function onRequest(event) {
   } else if (headers["origin"] !== "https://www.dylan-portfolio.dev") {
     event.respondWith(new Response("Access Not Allowed", { status: 403 }));
   }
+
   const cors = corsHndlr(headers, ["https://www.dylan-portfolio.dev"]);
-  console.log(cors);
+
   if (!cors) {
     event.respondWith(new Response("Access Not Allowed", { status: 403 }));
   }
@@ -30,5 +31,4 @@ function onRequest(event) {
 
 function onBeforeResponse(event) {
   const headers = getHeaders(event);
-  console.log("response", headers);
 }
